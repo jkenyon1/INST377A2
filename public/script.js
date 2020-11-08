d5ec1c8d612919f5634b8faba5933afba1ffb51e
jkenyon1 <62671170+jkenyon1@users.noreply.github.com>
2020-11-08 17:09:10 -0500

    update script.js

---
+++ public/script.js
@@ -0,0 +1,36 @@
+
+//fetch data and store it
+
+const list=[];
+
+function findMatches(wordToMatch, list){
+
+    return list.filter(place => {
+        const regex = new RegExp(wordToMatch,'gi');
+        return place.name.match(regex);
+    });
+}
+
+function displayMatches(){
+    const matchArray= findMatches(this.value, list);
+    const html=matchArray.map(place=>{
+        const regex= new RegExp(this.value, 'gi');
+        const placeName= list.name.replace(regex,`<span class="h1">${this.value}</span>`)
+        return `
+        <li>
+            <span class="name">${placeName}</span>
+            <span class="category">${place.category}</span>
+            <span class="address">${place.address_line_1},${place.city},
+                ${place.state},${place.zip}</span>
+            
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML= html;
+}
+
+const searchInput= document.quearySelector('.search');
+const suggestOutput= documnet.quearySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
