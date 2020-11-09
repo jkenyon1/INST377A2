4dc5e4959e7c4cf746da89a40957bcb8270305b5
Andrew Carroll <54873125+andcar23@users.noreply.github.com>
2020-11-08 20:46:33 -0500

    Display Matches Functional
    Fixed JS to display matches

--- public/script.js
+++ public/script.js
@@ -1,26 +1,29 @@
 
 //fetch data and store it
 const endpoint='https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const list=[];
+const list=[],
+    searchInput = document.querySelector('.search'),
+    filtered = document.querySelector('.filtered');
 
-fetch(endpoint).then(data =>data.json().then(data => list.push(...data)))
+fetch(endpoint)
+    .then(blob => blob.json()
+    .then(data => list.push(...data)))
 
-function findMatches(wordToMatch, list){
-
+function findMatches(wordToMatch, list) {
     return list.filter(place => {
         const regex = new RegExp(wordToMatch,'gi');
         return place.name.match(regex);
-    });
+    })
 }
 
 function displayMatches(){
-    const matchArray= findMatches(this.value, list);
-    const html=matchArray.map(place=>{
+    const matchArray = findMatches(this.value, list);
+    const html = matchArray.map(place => {
         const regex= new RegExp(this.value, 'gi');
-        const placeName= list.name.replace(regex,`<span class="h1">${this.value}</span>`)
+        /*const placeName= list.name.replace(regex,`<span class="h1">${this.value}</span>`)*/
         return `
         <li>
-            <span class="name">${placeName}</span>
+            <span class="name">${place.name}</span>
             <span class="category">${place.category}</span>
             <span class="address">${place.address_line_1},${place.city},
                 ${place.state},${place.zip}</span>
@@ -28,11 +31,9 @@ function displayMatches(){
         </li>
         `;
     }).join('');
-    suggestions.innerHTML= html;
+    filtered.innerHTML= html;
 }
 
-const searchInput= document.quearySelector('.search');
-const suggestOutput= documnet.quearySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
